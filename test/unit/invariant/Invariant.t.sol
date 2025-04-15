f877ab22c77b7bd2bcde8301bf6b2a7dc5aad85b
Julian Cabrera <juliancabmar@gmail.com>
2025-04-14 22:23:57 -0400

    ...

--- test/unit/invariant/Invariant.t.sol
+++ test/unit/invariant/Invariant.t.sol
@@ -12,15 +12,34 @@ contract Invariant is StdInvariant, Test {
     ERC20Mock poolToken;
     ERC20Mock weth;
 
+    int256 constant STARTING_X = 100e18; // starting amount of "POOLT"
+    int256 constant STARTING_Y = 50e18; // starting amount of "WETH"
+
     // this will be the pool factory that will be used to create pools
     PoolFactory poolFactory;
     // this will be the pool created by the factory (poolToken/weth)
     TSwapPool pool;
 
     function setUp() public {
+        // create the 2 tokens for the pool
         poolToken = new ERC20Mock("Pool Token", "POOLT");
         weth = new ERC20Mock("Wrapped Ether", "WETH");
+
+        // set up the pool factory and his resulting pool
         poolFactory = new PoolFactory(address(weth));
         pool = TSwapPool(poolFactory.createPool(address(poolToken)));
+
+        // mint those tokens for the pool
+        poolToken.mint(address(this), uint256(STARTING_X));
+        weth.mint(address(this), uint256(STARTING_Y));
+
+        // approve the pool to spend the tokens
+        poolToken.approve(address(pool), type(uint256).max);
+        weth.approve(address(weth), type(uint256).max);
+
+        // set the deposit into the pool
+        pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), uint64(block.timestamp));
     }
+
+    function statefulFuzz_constantProductFormulaStaysTheSame() public { }
 }
